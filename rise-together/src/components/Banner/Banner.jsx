eb6fc30a50482d52b4e8963e70f104999e53daa9
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-01-23 12:57:05 +0530

    Working on NavBar and Creating Basic Structure

---
+++ rise-together/src/components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple app with routing, a navbar, and cards.</p>
+    </div>
+  );
+};
+
+export default Banner;
