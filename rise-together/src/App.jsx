465526ae21bcc796aba595b0fe14e1ebd3b44eae
RangaJaladurgam <rangajaladurgam7258@gmail.com>
2025-01-26 11:31:42 +0530

    Added All Links to Ascend courses

--- rise-together/src/App.jsx
+++ rise-together/src/App.jsx
@@ -31,7 +31,7 @@ import AscendCourses from "./components/content/AscendCourses";
 function App() {
   const location = useLocation();
   return (
-    <>
+    <div >
       <TransitionGroup>
         <CSSTransition key={location.pathname} classNames="fade" timeout={300}>
       <div className="container">
@@ -39,7 +39,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/bizskills" element={<BizzSkills />} />
           <Route path="/career-edge" element={<CareerEdge />} />
-          <Route path="/ascend-course" element={<Home />} />
+          <Route path="/#ascend-course" element={<Home />} />
         </Routes>
       </div>
       </CSSTransition>
@@ -120,7 +120,7 @@ function App() {
           </CSSTransition>
         </TransitionGroup>
       </div>
-    </>
+    </div>
   );
 }
 const AppWrapper = () => (
--- rise-together/src/components/Home/Home.jsx
+++ rise-together/src/components/Home/Home.jsx
@@ -37,36 +37,48 @@ function Home() {
         </Link>
       </div>
       <div className="card-container">
-        <Link to="/ascend-course" className=" ascend-header">
-          <h3>AsCEnD Courses</h3>
+        <Link to="#ascend-course" className=" ascend-header">
+          <h3 className="">AsCEnD Courses</h3>
         </Link>
       </div>
-      <div className="card-container" id="/ascend-course">
-      <Link
-          to="/ascend-courses/advance-java"
-          className="nav-link"
-        >
+      <div className="card-container kyt-questions" id="#ascend-course">
+        <Link to="/ascend-courses/advance-java" className="nav-link">
           <Card title="AsCEnD Advanced Java Programming" />
         </Link>
-        <Link
-          to="/ascend-courses/java-mvc&web-dev"
-          className="nav-link"
-        >
+        <Link to="/ascend-courses/java-mvc&web-dev" className="nav-link">
           <Card title="AsCEnD Java MVC and Web Development" />
         </Link>
-        <Link
-          to="/ascend-courses/python-web&flask"
-          className="nav-link"
-        >
+        <Link to="/ascend-courses/python-web&flask" className="nav-link">
           <Card title="AsCEnD Python Web with Flask" />
         </Link>
-      <Link
-          to="/ascend-courses/python-web&flask"
-          className="nav-link"
-        >
+        <Link to="/ascend-courses/machine-learning" className="nav-link">
           <Card title="AsCEnD Machine Learning" />
         </Link>
-      </div>  
+        <Link to="/ascend-courses/node" className="nav-link">
+          <Card title="AsCEnD Node JS" />
+        </Link>
+        <Link to="/ascend-courses/mongo-db" className="nav-link">
+          <Card title="AsCEnD Mongo DB" />
+        </Link>
+        <Link to="/ascend-courses/test-automation" className="nav-link">
+          <Card title="AsCEnD Test Automation" />
+        </Link>
+        <Link to="/ascend-courses/azure-cloud" className="nav-link">
+          <Card title="AsCEnD Azure Cloud" />
+        </Link>
+        <Link to="/ascend-courses/adv-dotnet" className="nav-link">
+          <Card title="AsCEnD Advanced DotNet" />
+        </Link>
+        <Link to="/ascend-courses/datawarehouse" className="nav-link">
+          <Card title="AsCEnD Datawarehouse" />
+        </Link>
+        <Link to="/ascend-courses/devops" className="nav-link">
+          <Card title="AsCEnD Devops" />
+        </Link>
+        <Link to="/ascend-courses/bigdata" className="nav-link">
+          <Card title="AsCEnD Bigdata" />
+        </Link>
+      </div>
     </div>
   );
 }
